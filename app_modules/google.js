a21b292f999884b27215066be4b27b6850f513af
Gabriel Axel <guznik@gmail.com>
2017-02-27 14:52:07 +0200

    Implemented client for Google Cloud Vision

--- app_modules/google.js
+++ app_modules/google.js
@@ -1,5 +1,15 @@
+const config = require('config')
+const Vision = require('@google-cloud/vision');
+
+const vision = Vision({
+  projectId: config.get('google.projectId')
+});
+
+const terms = [ 'sea', 'ocean', 'body of water', 'coast' ]
+
 module.exports = {
   hasSurf: function(photoUrl,callback){
-    callback(null,false)
+    vision.detectLabels(photoUrl)
+      .then(results => callback(photoUrl, results[0].some(label => terms.includes(label))))
   }
 }
