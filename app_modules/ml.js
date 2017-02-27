4591226228695d1f3f864c17744ea88e92fcfed6
Shahar Solomianik <shahar@gnift.com>
2017-02-27 10:49:46 +0200

    clarifai switch

--- app_modules/ml.js
+++ app_modules/ml.js
@@ -1,5 +1,18 @@
+var config = require('config')
+
+var clarifai = require('../app_modules/clarifai')
+
 module.exports = {
   hasSurf: function(photoUrl,callback){
-    callback(null,false)
+
+    switch(config.get('app.ml_provider')){
+      case 'clarifai':
+        clarifai.hasSurf(photoUrl,callback);
+        break;
+      default:
+        callback(null,false)
+    }
+
+
   }
 }
