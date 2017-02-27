76a2e0128e82002970040c32abba8721d811edad
alexs@tikalk.com <alexs@tikalk.com>
2017-02-27 11:11:46 +0200

    clarifai integration

--- app_modules/clarifai.js
+++ app_modules/clarifai.js
@@ -1,5 +1,36 @@
+var _ = require('lodash');
+var config = require('config');
+
+// Require the client
+var Clarifai = require('clarifai');
+
 module.exports = {
-  hasSurf: function(photoUrl,callback){
-    callback(null,false)
-  }
+    hasSurf: function (photoUrl, callback) {
+
+        // instantiate a new Clarifai app passing in your clientId and clientSecret
+        var app = new Clarifai.App(config.get.clarifai.api_key, config.get.clarifai.secret);
+
+        app.models.predict(Clarifai.GENERAL_MODEL, photoUrl).then(
+                function (response) {
+                    console.log("Response");
+
+                    var concepts = JSON.parse(JSON.stringify(response.outputs[0].data.concepts));
+                    var keywords = config.get.clarifai.good_keywords;
+                            
+                    var isHasSurf = false;
+                    _.each(concepts, function (v) {
+                        if (keywords.indexOf(v.name) > -1) {
+                            isHasSurf = true;
+                        }
+                    });
+                    
+                    callback(null, isHasSurf);
+
+                },
+                function (err) {
+                    console.error(err);
+                }
+        );
+        
+    }
 }
