d0d7b0ebffc71c94a5572a21d7657f98e72f02bc
Shahar Solomianik <shahar@gnift.com>
2017-02-27 15:56:00 +0200

    clarifai2

--- app_modules/ml.js
+++ app_modules/ml.js
@@ -1,6 +1,7 @@
 var config = require('config')
 
 var clarifai = require('../app_modules/clarifai')
+var clarifai2 = require('../app_modules/clarifai2')
 var google = require('../app_modules/google')
 
 module.exports = {
@@ -9,6 +10,9 @@ module.exports = {
     switch(config.get('app.ml_provider')){
       case 'clarifai':
         clarifai.hasSurf(photoUrl,callback);
+        break;
+      case 'clarifai2':
+        clarifai2.hasSurf(photoUrl,callback);
         break;
       case 'google':
         google.hasSurf(photoUrl,callback);
